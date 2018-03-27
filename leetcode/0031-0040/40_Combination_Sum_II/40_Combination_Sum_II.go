eb99140b736d2de344fb97a5fdb0df638582c7d5
Charles Wu <No1CharlesWu@163.com>
2018-03-27 23:48:14 +0800

    40 优化修改代码，无需去重

--- leetcode/0031-0040/40_Combination_Sum_II/40_Combination_Sum_II.go
+++ leetcode/0031-0040/40_Combination_Sum_II/40_Combination_Sum_II.go
@@ -22,16 +22,20 @@ func combinationSum2(candidates []int, target int) [][]int {
 
 func dfs(candidates []int, target int, arr []int, res *[][]int, index int) {
 	for i := index; i < len(candidates); i++ {
+		if i > index && candidates[i] == candidates[i-1] {
+			continue
+		}
 		newTarget := target - candidates[i]
 		arr = append(arr, candidates[i])
 		if newTarget > 0 {
 			dfs(candidates, newTarget, arr, res, i+1)
-		} else if newTarget == 0 && !inRange(arr, *res) {
+		} else if newTarget == 0 {
 			*res = append(*res, append([]int{}, arr...))
 		}
 		arr = (arr)[:len(arr)-1]
 	}
 }
+
 func inRange(a []int, r [][]int) bool {
 	for i := 0; i < len(r); i++ {
 		if equal(a, r[i]) {
